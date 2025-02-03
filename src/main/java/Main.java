011f0439886a99aeea75ce365325b00ae8ade184
EthanLiu2 <etliu1@s.sfusd.edu>
2025-02-02 22:42:21 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public void setup()
   System.out.println("there are " + lines.length + " lines");
   for (int i=0; i < lines.length; i++)
   {
+
     if (palindrome(lines[i])==true)
     {
       System.out.println(lines[i] + " IS a palindrome.");
@@ -32,18 +33,27 @@ public String reverse(String str)
   for (int i=str.length()-1; i>=0; i--) {
     sNew=sNew+str.substring(i, i+1);
   }
-  
+
   return sNew;
 }
-public String noSpaces(String sWord){
+public String noSpaces(String sWord) {
   String s="";
-  for(int i=0;i<sWord.length();i++){
-    if(!sWord.substring(i,i+1).equals(" ")){
-     s= s+sWord.substring(i,i+1);
+  for (int i=0; i<sWord.length(); i++) {
+    if (!sWord.substring(i, i+1).equals(" ")) {
+      s= s+sWord.substring(i, i+1);
     }
   }
   return s;
 }
-public String noCapitals(String sWord){
+public String noCapitals(String sWord) {
   return sWord.toLowerCase();
+}
+public String onlyLetters(String sString) {
+  String s="";
+  for (int i=0; i<sString.length(); i++) {
+    if (Character.isLetter(sString.charAt(i))==true) {
+      s=s+sString.charAt(i);
+    }
+  }
+  return s;
 }
