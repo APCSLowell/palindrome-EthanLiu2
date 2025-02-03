043881b4ab3cc268335d66d82e95b865726676e1
EthanLiu2 <etliu1@s.sfusd.edu>
2025-02-02 23:16:28 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -3,77 +3,67 @@ import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class PalindromeChecker {
-public void tester()
-{
-  //String lines[] = loadStrings("palindromes.txt");
-  String[] lines = new String[6]; 
-    try{
-        File myFile = new File("palindromes.txt");
-        Scanner myReader = new Scanner(myFile);
-        int counter = 0;
-        while (myReader.hasNextLine()) {
-            String data = myReader.nextLine();
-            lines[counter] = data;
-            counter++;
-        }
-        myReader.close();
+  public void tester()
+  {
+    //String lines[] = loadStrings("palindromes.txt");
+    String[] lines = new String[6];
+    try {
+      File myFile = new File("palindromes.txt");
+      Scanner myReader = new Scanner(myFile);
+      int counter = 0;
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        lines[counter] = data;
+        counter++;
+      }
+      myReader.close();
     }
     catch (FileNotFoundException e) {
-        System.out.println("An error occurred.");
-        e.printStackTrace();
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
-  System.out.println("there are " + lines.length + " lines");
-  for (int i=0; i < lines.length; i++) 
+    System.out.println("there are " + lines.length + " lines");
+    for (int i=0; i < lines.length; i++)
+    {
+      if (palindrome(lines[i])==true)
+      {
+        System.out.println(lines[i] + " IS a palindrome.");
+      } else
+      {
+        System.out.println(lines[i] + " is NOT a palindrome.");
+      }
+    }
+  }
+  public boolean palindrome(String word) //check if is palindrome
   {
-    if(palindrome(lines[i])==true)
-    {
-      System.out.println(lines[i] + " IS a palindrome.");
-    }
-    else
-    {
-      System.out.println(lines[i] + " is NOT a palindrome.");
-    }
+    String every = onlyLetters(noCapitals(noSpaces(word)));
+    String backwards = reverse(every);
+    return every.equals(backwards);
   }
-}
-public boolean palindrome(String word)
-{
-  String s="";
-  for (int i=word.length()-1; i>=0; i--) {
-    s=s+word.substring(i, i+1);
-  }
-  if (s.equals(word)) {
-    return true;
-  }
-  return false;
-}
+  public String reverse(String word) //make reverse
+  {
+    String sNew = new String();
+    for (int i = word.length()-1; i >= 0; i--)
+      sNew = sNew + word.charAt (i);
 
-public String reverse(String str)
-{
-  String sNew = new String();
-  for (int i=str.length()-1; i>=0; i--) {
-    sNew=sNew+str.substring(i, i+1);
+    return sNew;
   }
+  public String noCapitals(String sWord) { //return string with all lowercase
+    return sWord.toLowerCase();
+  }
+  public String noSpaces(String sWord) { //return string with no spaces
+    String s = "";
+    for (int i = 0; i < sWord.length(); i++)
+      if (!sWord.substring (i, i+1).equals (" "))
+        s = s + sWord.charAt(i);
 
-  return sNew;
-}
-public String noSpaces(String sWord) {
-  String s="";
-  for (int i=0; i<sWord.length(); i++) {
-    if (!sWord.substring(i, i+1).equals(" ")) {
-      s= s+sWord.substring(i, i+1);
-    }
+    return s;
   }
-  return s;
-}
-public String noCapitals(String sWord) {
-  return sWord.toLowerCase();
-}
-public String onlyLetters(String sString) {
-  String s="";
-  for (int i=0; i<sString.length(); i++) {
-    if (Character.isLetter(sString.charAt(i))==true) {
-      s=s+sString.charAt(i);
-    }
+  public String onlyLetters(String word) { //return only letters
+    String s = "";
+    for (int i = 0; i < word.length(); i++)
+      if (Character.isLetter(word.charAt(i)))
+        s = s + word.charAt(i);
+    return s;
   }
-  return s;
 }
