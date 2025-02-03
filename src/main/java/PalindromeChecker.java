c65479d4c10de615c4d72892636ca0176dbeef34
EthanLiu2 <etliu1@s.sfusd.edu>
2025-02-02 22:53:49 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,13 +37,43 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  //your code here
+  String s="";
+  for (int i=word.length()-1; i>=0; i--) {
+    s=s+word.substring(i, i+1);
+  }
+  if (s.equals(word)) {
+    return true;
+  }
   return false;
 }
+
 public String reverse(String str)
 {
-    String sNew = new String();
-    //your code here
-    return sNew;
+  String sNew = new String();
+  for (int i=str.length()-1; i>=0; i--) {
+    sNew=sNew+str.substring(i, i+1);
+  }
+
+  return sNew;
 }
+public String noSpaces(String sWord) {
+  String s="";
+  for (int i=0; i<sWord.length(); i++) {
+    if (!sWord.substring(i, i+1).equals(" ")) {
+      s= s+sWord.substring(i, i+1);
+    }
+  }
+  return s;
+}
+public String noCapitals(String sWord) {
+  return sWord.toLowerCase();
+}
+public String onlyLetters(String sString) {
+  String s="";
+  for (int i=0; i<sString.length(); i++) {
+    if (Character.isLetter(sString.charAt(i))==true) {
+      s=s+sString.charAt(i);
+    }
+  }
+  return s;
 }
