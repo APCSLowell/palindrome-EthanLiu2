ec1b1e7de65a4be6abe02259d14fe21d5f8edfb7
EthanLiu2 <etliu1@s.sfusd.edu>
2025-02-02 22:29:38 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,14 @@
 public void setup()
 {
-  String lines[] = new String[]{"test","rotator","rewriter","nurses run","Madam, I'm Adam!","A Man! A Plan! A Canal! Panama!"};
+  String lines[] = new String[]{"test", "rotator", "rewriter", "nurses run", "Madam, I'm Adam!", "A Man! A Plan! A Canal! Panama!"};
 
   System.out.println("there are " + lines.length + " lines");
-  for (int i=0; i < lines.length; i++) 
+  for (int i=0; i < lines.length; i++)
   {
-    if(palindrome(lines[i])==true)
+    if (palindrome(lines[i])==true)
     {
       System.out.println(lines[i] + " IS a palindrome.");
-    }
-    else
+    } else
     {
       System.out.println(lines[i] + " is NOT a palindrome.");
     }
@@ -18,20 +17,33 @@ public void setup()
 public boolean palindrome(String word)
 {
   String s="";
-  for(int i=word.length()-1;i>=0;i--){
-  s=s+word.substring(i,i+1);
+  for (int i=word.length()-1; i>=0; i--) {
+    s=s+word.substring(i, i+1);
   }
-  if(s.equals(word)){
+  if (s.equals(word)) {
     return true;
   }
   return false;
-
 }
+
 public String reverse(String str)
 {
-    String sNew = new String();
-    for(int i=str.length()-1;i>=0;i--){
-  sNew=sNew+str.substring(i,i+1);
+  String sNew = new String();
+  for (int i=str.length()-1; i>=0; i--) {
+    sNew=sNew+str.substring(i, i+1);
   }
-    return sNew;
+  
+  return sNew;
+}
+public String noSpaces(String sWord){
+  String s="";
+  for(int i=0;i<sWord.length();i++){
+    if(!sWord.substring(i,i+1).equals(" ")){
+     s= s+sWord.substring(i,i+1);
+    }
+  }
+  return s;
+}
+public String noCapitals(String sWord){
+  return sWord.toLowerCase();
 }
